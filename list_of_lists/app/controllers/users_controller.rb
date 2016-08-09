bb8d13b4731e36ddba2022cce305ebfd12cfbc6e
Castelone <Castelone@live.com>
2016-08-09 15:01:47 -0600

    userprofile

--- list_of_lists/app/controllers/users_controller.rb
+++ list_of_lists/app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 	def user_params
 		params.require(:user).permit(:username, :email, :first_name, :last_name, :bio, :password, :img_url)
+    end
+    
+    def show
+        @user = User.find_by_id(params[:id])
+        render :show
+    end
+    
 end
