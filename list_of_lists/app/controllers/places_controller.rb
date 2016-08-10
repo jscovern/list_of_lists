76f43bd0f50fda99eceac1afa99d774768dde921
Mark Johnson <markjohnson303@gmail.com>
2016-08-10 16:29:04 -0600

    movies and places adding works

--- list_of_lists/app/controllers/places_controller.rb
+++ list_of_lists/app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 	
 	def create
     	@place = Place.create(place_params)
-    	redirect_to "/lists/#{@places.list_id}"
+    	redirect_to "/lists/#{@place.list_id}"
     end
 
     def new
@@ -18,18 +18,18 @@ class PlacesController < ApplicationController
     def update
     	@place = Place.find_by_id(params[:id])
     	@place.update_attributes(place_params)
-    	redirect_to "/lists/#{@places.list_id}"
+    	redirect_to "/lists/#{@place.list_id}"
     end
 
     def destroy
     	@place = Place.find_by_id(params[:id])
     	@place.destroy
-    	redirect_to "/lists/#{@places.list_id}"
+    	redirect_to "/lists/#{@place.list_id}"
 	end 
 
 private
 	def place_params
-		params.require(:place).permit(:name, :address, :latitude, :longitude, :description, :img_url, :list_id)
+		params.permit(:name, :address, :latitude, :longitude, :description, :img_url, :list_id)
 	end
 end
 
