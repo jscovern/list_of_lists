3009213d5cda56989f8a4a1500404442f864bfae
James Scovern <jamie.scovern@gmail.com>
2016-08-11 19:26:22 -0600

    followers query

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
 	def index
 		@features = List.last(10).reverse
+		if session[:user_id] != nil
+			@followed = Following.where(:follower_id => session[:user_id])
+			#.order(:created_at => "desc").limit(10)
+			@listusers = ListUser.where(:user_id => @followed.map(&:user_id))
+			# @lists = List.where(:id => @list_ids.map(&:list_id))
+		end
 		render :index
 	end
     
